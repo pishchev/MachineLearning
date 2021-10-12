87cb27a48a627b7ec8a86ce40b052d9681c99efd
pishchev <71860222+pishchev@users.noreply.github.com>
2021-10-12 10:48:55 +0700

    Add files via upload

---
+++ KNN/model.py
@@ -0,0 +1,33 @@
+import numpy as np
+import kernels
+
+import utils
+
+class Model:
+    def __init__(self, weights_size, max_distance, kernel=kernels.Gauss, pow_distance=2):
+        self.weights = np.zeros(weights_size, int)
+        self.pow_distance = pow_distance
+        self.kernel = kernel
+        self.max_distance = max_distance
+        
+    def non_zero_weights(self):
+        nzarray = []
+        for index in range(len(self.weights)):
+            if (self.weights[index]!= 0):
+                nzarray.append(index)
+        return np.array(nzarray)
+
+    def predicts(self, test_X, train_X, train_Y):
+        preds = []
+        for sample in test_X:
+            preds.append(self.predict(sample, train_X, train_Y))
+        return np.array(preds)
+
+    def predict(self, sample, train_X, train_Y):
+        resVec = np.zeros(train_Y.shape[0])
+        for index in range(len(train_X)):
+            label = train_Y[index]
+            train_sample = train_X[index]
+            weight = self.weights[index]
+            resVec[label] +=  weight * self.kernel(utils.dist(train_sample,sample,self.pow_distance) / self.max_distance)
+        return np.argmax(resVec)
--- KNN/utils.py
+++ KNN/utils.py
@@ -1,21 +1,27 @@
 from scipy.spatial import distance
 import numpy as np
 
-train_data = 0
-
 def dist(sample1,sample2,n=2):
     return distance.minkowski(sample1,sample2,n)
 
-def max_distance(n=2):
+def max_distance(dataset, n=2):
     mx = 0
-    for x in train_data:
-        for y in train_data:
+    for x in dataset:
+        for y in dataset:
             mx = max(mx, dist(x,y,n))
     return mx
 
-def non_zero_weigth(weights):
-    nzarray = []
-    for index in range(len(weights)):
-        if (weights[index]!= 0):
-            nzarray.append(index)
-    return np.array(nzarray)
+def wrong_indexes(predicts, truth):
+    wrong_ind =[]
+    for index in range(len(predicts)):
+        if predicts[index] != truth[index]:
+            wrong_ind.append(index)
+    return wrong_ind
+    
+def accuracy(predicts, ground_truth):
+    truth = 0
+    for index in range(len(predicts)):
+        if predicts[index] == ground_truth[index]:
+            truth +=1 
+    print(str(truth) +"/" + str(len(predicts)))
+    print("Accuracy: "+str(truth/len(predicts)))
