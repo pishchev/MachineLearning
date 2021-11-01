3a866293db3151e557d4efa2a01d73f5a66d8f09
pishchev <71860222+pishchev@users.noreply.github.com>
2021-11-01 19:36:04 +0700

    zeroes+broadcast

--- KNN/model.py
+++ KNN/model.py
@@ -10,12 +10,15 @@ class Model:
         self.kernel = kernel
         self.max_distance = max_distance
         
-    def non_zero_weights(self):
-        nzarray = []
+    def non_zero_weights(self, is_zero):
+        array = []
         for index in range(len(self.weights)):
-            if (self.weights[index] != 0):
-                nzarray.append(index)
-        return np.array(nzarray)
+            if (not is_zero and self.weights[index] != 0):
+                array.append(index)
+            elif (is_zero and self.weights[index] == 0):
+                array.append(index)
+
+        return np.array(array)
 
     def predicts(self, test_X, train_X, train_Y):
         preds = list(map(lambda x: self.predict(x, train_X, train_Y), test_X))
@@ -23,9 +26,11 @@ class Model:
 
     def predict(self, sample, train_X, train_Y):
         resVec = np.zeros(train_Y.shape[0])
-        for index in range(len(train_X)):
-            label = train_Y[index]
-            train_sample = train_X[index]
-            weight = self.weights[index]
-            resVec[label] +=  weight * self.kernel(utils.dist(train_sample, sample, self.pow_distance) / self.max_distance)
-        return np.argmax(resVec)
+
+        diffs = np.array(list(map(lambda x: self.kernel(utils.dist(x, sample, self.pow_distance) / self.max_distance), train_X)))
+        weights = self.weights * diffs
+        nresVec = np.zeros(train_Y.shape[0])
+        for i in range(len(train_Y)):
+            nresVec[i] = np.sum( weights[ i == train_Y] )
+
+        return np.argmax(nresVec)
